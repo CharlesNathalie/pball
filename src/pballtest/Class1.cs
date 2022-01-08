3eb1360c6cd86031990197095ae4a8c2e31a0ae1
CharlesNathalie <96335304+CharlesNathalie@users.noreply.github.com>
2022-01-08 07:50:26 -0400

    update

--- src/pballtest/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿{
-    //"pballurl": "https://pball.azurewebsites.net/",
-    "pballurl": "https://localhost:7072/",
-  "APISecret": "Someting THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING",
-  //"pballDB": "Server=tcp:pball.database.windows.net,1433;Initial Catalog=pball;Persist Security Info=False;User ID=pball_user;Password=Veronique93!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-  "pballDB": "Server=.\\sqlexpress;Database=pball;Trusted_Connection=True;MultipleActiveResultSets=true"
-}
